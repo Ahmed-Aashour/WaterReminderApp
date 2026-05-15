6a4be3789e19573115000b1983cc4543c3361666
Ahmed-Aashour <es-AhmedAshour2024@alexu.edu.eg>
2026-05-16 01:49:37 +0300

    Refactor: Rename Application name to WaterReminder and update MainActivity

--- app/src/main/java/android/waterreminder/ui/MainActivity.kt
+++ app/src/main/java/android/waterreminder/ui/MainActivity.kt
@@ -17,37 +17,18 @@
 package android.waterreminder.ui
 
 import android.os.Bundle
-import android.waterreminder.ui.theme.MyApplicationTheme
 import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
+import androidx.compose.material3.Text
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge(
-            statusBarStyle = SystemBarStyle.auto(
-                lightScrim = android.graphics.Color.TRANSPARENT,
-                darkScrim = android.graphics.Color.TRANSPARENT
-            )
-        )
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MainNavigation()
-                }
-            }
+            Text(text = "Water Reminder App Environment Ready!")
         }
     }
 }
