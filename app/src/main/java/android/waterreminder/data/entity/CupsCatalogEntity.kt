35f7cebb933d70d474284a720a35dbbf24433260
Ahmed-Aashour <es-AhmedAshour2024@alexu.edu.eg>
2026-06-05 19:16:41 +0300

    feat(data): add cups catalog entity matching cups_catalog table schema

---
+++ app/src/main/java/android/waterreminder/data/entity/CupsCatalogEntity.kt
@@ -0,0 +1,10 @@
+package android.waterreminder.data.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "cups_catalog")
+data class CupsCatalogEntity(
+    @PrimaryKey
+    val amountMl: Int // Using the capacity volume as the primary key since duplicates aren't allowed
+)
