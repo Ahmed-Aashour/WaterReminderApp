ec3ddada2c914e0f504c04fd1176f63280f3b18e
Ahmed-Aashour <es-AhmedAshour2024@alexu.edu.eg>
2026-06-07 04:27:05 +0300

    refactor(arch): resolve hiltViewModel deprecation warning by updating import scopes

--- app/src/main/java/android/waterreminder/ui/settings/SettingsRoute.kt
+++ app/src/main/java/android/waterreminder/ui/settings/SettingsRoute.kt
@@ -3,7 +3,7 @@ package android.waterreminder.ui.settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
 @Composable
